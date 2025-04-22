c636444a782fb77d9cf93e409b8e6a906454fa74
Bthxtly <bthxtly@gmail.com>
2025-04-22 20:13:10 +0800

    generate map from assets/map.json

--- map.cpp
+++ map.cpp
@@ -11,29 +11,29 @@
 
 using json = nlohmann::json;
 
-std::map<std::string, elements> string2elements = {
-    {"WALL", WALL},
-    {"SPACE", SPACE},
-    {"LAVA", LAVA},
-    {"DOOR", DOOR},
-    {"UP_BLOCK", UP_BLOCK},
-    {"DOWN_BLOCK", DOWN_BLOCK},
-    {"SLIME", SLIME},
-    {"SKELETON", SKELETON},
-    {"BAT", BAT},
-    {"APOSTLE", APOSTLE},
-    {"BEELZEBUB", BEELZEBUB},
-    {"SMALL_BOTTLE", SMALL_BOTTLE},
-    {"BIG_BOTTLE", BIG_BOTTLE},
-    {"SWORD", SWORD},
-    {"SHIELD", SHIELD},
-    {"LIFE_GEM", LIFE_GEM},
-    {"KEY", KEY},
-    {"HERO", HERO},
-};
+Map::Map(std::string path) {
+  std::map<std::string, elements> string2elements = {
+      {"WALL", WALL},
+      {"SPACE", SPACE},
+      {"LAVA", LAVA},
+      {"DOOR", DOOR},
+      {"UP_BLOCK", UP_BLOCK},
+      {"DOWN_BLOCK", DOWN_BLOCK},
+      {"SLIME", SLIME},
+      {"SKELETON", SKELETON},
+      {"BAT", BAT},
+      {"APOSTLE", APOSTLE},
+      {"BEELZEBUB", BEELZEBUB},
+      {"SMALL_BOTTLE", SMALL_BOTTLE},
+      {"BIG_BOTTLE", BIG_BOTTLE},
+      {"SWORD", SWORD},
+      {"SHIELD", SHIELD},
+      {"LIFE_GEM", LIFE_GEM},
+      {"KEY", KEY},
+      {"HERO", HERO},
+  };
 
-Map::Map() {
-  std::ifstream file("./assets/map.json");
+  std::ifstream file(path);
   if (!file) {
     std::cerr << "Could not open ./assets/map.json.\n";
   }
@@ -42,33 +42,38 @@ Map::Map() {
   file >> j;
 
   // Read the legend
-  std::map<char, elements> char2element;
+  std::map<std::string, elements> char2element;
   for (auto &[symbolStr, meaningStr] : j["legend"].items()) {
-    char symbol = symbolStr[0];
     std::string meaning = meaningStr.get<std::string>();
-    char2element[symbol] = string2elements[meaning];
+    char2element[symbolStr] = string2elements[meaning];
   }
 
   // Load data
   height = j["height"];
   width = j["width"];
+  floor = j["floor"];
 
   // Load the map
-  for (const auto &rowStr : j["tiles"]) {
-    std::string row = rowStr.get<std::string>();
-    std::vector<elements> mapRow;
-    for (auto ch : row) {
-      mapRow.push_back(char2element[ch]);
+  for (auto &[floor_num_str, floor_data] : j["floors"].items()) {
+    std::vector<std::vector<elements>> mapFloor;
+    for (const auto &rowStr : floor_data) {
+      std::string row = rowStr.get<std::string>();
+      std::vector<elements> mapRow;
+      for (int i = 0; i < width * 3; i += 3) {
+        mapRow.push_back(char2element[row.substr(i, 3)]);
+      }
+      mapFloor.push_back(mapRow);
     }
-    map.push_back(mapRow);
+    map.push_back(mapFloor);
   }
 
-  std::cout << height << " " << width << "\n";
-  // // Print the map
-  // for (const auto &row : map) {
-  //   for (auto tile : row) {
-  //     std::cout << tile;
+  //   // Print the map
+  //   for (const auto &floor : map) {
+  //     for (auto &row : floor) {
+  //       for (auto &tile : row)
+  //         std::cout << tile;
+  //       std::cout << "\n";
+  //     }
+  //     std::cout << "\n";
   //   }
-  //   std::cout << "\n";
-  // }
 }
--- map.hpp
+++ map.hpp
@@ -1,19 +1,30 @@
 #ifndef _MAP_HPP_
 #define _MAP_HPP_
 
+#include <string>
 #include <vector>
 
 #include "elements.hpp"
 
 class Map {
 public:
-  Map();
+  Map() {};
+  Map(std::string path);
+  Map &operator=(const Map &other) = default;
   ~Map() {}
+
+  int getWidth() { return width; }
+  int getHeight() { return height; }
+  elements getCell(int w, int h, int floor) { return map[floor][h][w]; }
+  void setCell(int w, int h, int floor, elements ele) {
+    map[floor][h][w] = ele;
+  }
 
 private:
   int width;
   int height;
-  std::vector<std::vector<elements>> map;
+  int floor;
+  std::vector<std::vector<std::vector<elements>>> map;
 };
 
 #endif // !_MAP_HPP_

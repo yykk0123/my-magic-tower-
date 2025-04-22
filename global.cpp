ba8499722edfc0910729246419573cc6a8f2a1b5
Bthxtly <bthxtly@gmail.com>
2025-04-22 20:15:14 +0800

    global variables

---
+++ global.cpp
@@ -0,0 +1,13 @@
+#include "global.hpp"
+
+int end;
+Hero hero;
+
+Map map_original("./assets/map.json");
+Map map;
+
+Monster slime;
+Monster skeleton;
+Monster bat;
+Monster apostle;
+Monster beelzebub;
---
+++ global.hpp
@@ -0,0 +1,16 @@
+#ifndef _GLOBAL_HPP_
+#define _GLOBAL_HPP_
+
+#include "hero.hpp"
+#include "map.hpp"
+#include "monster.hpp"
+
+extern int end;
+
+extern Hero hero;
+
+extern Monster slime, skeleton, bat, apostle, beelzebub;
+
+extern Map map_original, map;
+
+#endif // _GLOBAL_HPP_
